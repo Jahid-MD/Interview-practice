1557e76c9b85f78b9f31bdec757e5a30cd9acbcc
Jahid-md <jahidmohd017@gmail.com>
2024-08-09 18:23:00 +0530

    module included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './components/first-component/first.component';
+import { FirstModule } from './modules/first.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponent],
+  imports: [RouterOutlet, FirstComponent,FirstModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/modules/first.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { ModOneComponent } from "../components/first-component/components/mod-one.component";
+
+@NgModule({
+    declarations: [ModOneComponent],
+    exports: [ModOneComponent],
+    imports:[]
+})
+export class FirstModule {
+
+}
